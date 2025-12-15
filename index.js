f51b94491f82440b8f426ba77486822d45e999b4
Sunny-kr-01 <2405984@kiit.ac.in>
2025-12-15 12:51:24 +0530

    use urlencoded({extended:false})

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const fs=require('fs')
 const users = require('./MOCK_DATA.json')
 const app = express();
 
-app.route('/josn/users/:id')
+app.use(express.urlencoded({extended:false}))
+
+app.route('/json/users/:id')
 .get((req,res)=>{
     const id=Number(req.params.id);
     const user= users.find((user)=>{
@@ -12,10 +14,14 @@ app.route('/josn/users/:id')
     res.json(user);
 })
 .patch((req,res)=>{
-
+    // updqate 
 })
 .delete((req,res)=>{
-
+    const id= req.params.id;
+    const body = req.body;
+    const user_to_del= users.find(user=>user.id==id);
+    console.log(user_to_del);
+    res.json({status:"Pending"})
 })
 
 app.get('/',(req,res)=>{
@@ -45,10 +51,9 @@ app.get('/users/:id',(req,res)=>{
     res.send(user.first_name+" "+user.last_name)
 })
 
-app.use(express.urlencoded({extended:false}))
-
 app.post('/json/users',(req,res)=>{
     const body = req.body;
+    console.log(body)
     users.push({...body,id:users.length+1}); 
     fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
         res.json({sttus : "success" , id :users.length});
